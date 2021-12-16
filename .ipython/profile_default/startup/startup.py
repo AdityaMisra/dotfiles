12094710bde7b50772c1e8936d07c0eb6569036b
Aditya Misra <amisra14@yahoo.co.uk>
2021-12-16 19:17:37 +0530

    Create startup.py

---
+++ .ipython/profile_default/startup/startup.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+import this
+try:
+    import json
+except:
+    pass
+
+try:
+    import requests
+except:
+    pass
+
+try:
+    from ipdb import runcall
+except:
+    pass
